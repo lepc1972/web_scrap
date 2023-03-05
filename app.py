38ab9627c03a7ee25f5e5ed8893753195f831516
lepc1972 <githublepc@gmail.com>
2023-03-05 14:32:16 -0500

    woooorks

--- app.py
+++ app.py
@@ -1,38 +1,36 @@
-# Importar módulos
 import requests
 import csv
-import time
 from bs4 import BeautifulSoup
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='.')
+
 # Dirección de la página web
 url = "http://quotes.toscrape.com/"
+
 # Ejecutar GET-Request
 response = requests.get(url)
+
 # Analizar sintácticamente el archivo HTML de BeautifulSoup del texto fuente
 html = BeautifulSoup(response.text, 'html.parser')
+
 # Extraer todas las citas y autores del archivo HTML
 quotes_html = html.find_all('span', class_="text")
 authors_html = html.find_all('small', class_="author")
-# Crear una lista de las citas
-quotes = list()
-for quote in quotes_html:
-    quotes.append(quote.text)
-# Crear una lista de los autores
-authors = list()
-for author in authors_html:
-    authors.append(author.text)
-# Para hacer el test: combinar y mostrar las entradas de ambas listas
-for t in zip(quotes, authors):
-    print(t)
-# Guardar las citas y los autores en un archivo CSV en el directorio actual
-# Abrir el archivo con Excel / LibreOffice, etc.
-with open('./zitate.csv', 'w') as csv_file:
-    csv_writer = csv.writer(csv_file, dialect='excel')
-    csv_writer.writerows(zip(quotes, authors))
 
-while True:
-    time.sleep(3600)  # sleep for one hour
+# Crear una lista de las citas y autores
+quotes_and_authors = list()
+for quote, author in zip(quotes_html, authors_html):
+    quotes_and_authors.append((quote.text, author.text))
 
-port = int(os.environ.get("PORT", 3000))
-app.run(debug=True, port=port, host="0.0.0.0")
+# Ruta principal de la aplicación web
+@app.route('/')
+def index():
+    # Renderizar la plantilla HTML 'index.html' y pasar las citas y autores como argumentos
+    return render_template('index.html', quotes_and_authors=quotes_and_authors)
 
+if __name__ == '__main__':
+    # Ejecutar la aplicación Flask en el puerto definido por la variable de entorno 'PORT'
+    # port = int(os.environ.get("PORT", 3000))
+    app.run(debug=True, port=3000, host="0.0.0.0")
 
