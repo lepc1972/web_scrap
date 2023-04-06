c7c759e393c3854c2281bcaf96fa4bfde2cf5214
lepc1972 <githublepc@gmail.com>
2023-04-05 20:08:17 -0500

    retaken for gcp

--- app.py
+++ app.py
@@ -1,36 +1,50 @@
-import requests
-import csv
-from bs4 import BeautifulSoup
 from flask import Flask, render_template
+from bs4 import BeautifulSoup
+import csv
+import logging
+import os
+import requests
+i  # Imports
 
+# Flask application
 app = Flask(__name__, template_folder='.')
 
-# Dirección de la página web
+# URL of the web page
 url = "http://quotes.toscrape.com/"
 
-# Ejecutar GET-Request
+# Execute GET-Request
 response = requests.get(url)
+logging.info("GET-Request was successful")
 
-# Analizar sintácticamente el archivo HTML de BeautifulSoup del texto fuente
+# Parse the HTML file of BeautifulSoup from the source text
 html = BeautifulSoup(response.text, 'html.parser')
 
-# Extraer todas las citas y autores del archivo HTML
+# Extract all the quotes and authors from the HTML file
 quotes_html = html.find_all('span', class_="text")
 authors_html = html.find_all('small', class_="author")
 
-# Crear una lista de las citas y autores
+# Create a list of the quotes and authors
 quotes_and_authors = list()
 for quote, author in zip(quotes_html, authors_html):
     quotes_and_authors.append((quote.text, author.text))
 
-# Ruta principal de la aplicación web
+# Main route of the web application
+
+
 @app.route('/')
 def index():
-    # Renderizar la plantilla HTML 'index.html' y pasar las citas y autores como argumentos
+    # Render the HTML template 'index.html' and pass the quotes and authors as arguments
     return render_template('index.html', quotes_and_authors=quotes_and_authors)
 
-if __name__ == '__main__':
-    # Ejecutar la aplicación Flask en el puerto definido por la variable de entorno 'PORT'
-    # port = int(os.environ.get("PORT", 3000))
-    app.run(debug=True, port=8080, host="0.0.0.0")
 
+if __name__ == '__main__':
+    # Execute the Flask application on the port defined by the environment variable 'PORT'
+    # port = int(os.environ.get("PORT", 3000))
+    # Get the port number from the environment variable PORT
+    port = int(os.getenv('PORT', 8080))
+
+    # Print the port number
+    print("Starting app on port %d" % port)
+
+    # Run the app on the port number
+    app.run(debug=True, port=port, host="
